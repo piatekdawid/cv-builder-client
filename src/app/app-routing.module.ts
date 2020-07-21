b468fb27356a9cff70a7f6fc10d9b63f1ba52c20
Zugemibo <41533197+Zugemibo@users.noreply.github.com>
2020-07-21 08:01:44 +0200

    Added routing, http service.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {ExperienceComponent} from './experience/experience.component';
+import {EducationComponent} from './education/education.component';
+import {PersonalComponent} from './personal/personal.component';
+import {SkillComponent} from './skill/skill.component';
+import {LanguageComponent} from './language/language.component';
+import {CourseComponent} from './course/course.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'personal', component: PersonalComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'skill', component: SkillComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'language', component: LanguageComponent },
+  { path: 'course', component: CourseComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PersonalComponent } from './personal/personal.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { HobbyComponent } from './hobby/hobby.component';
+import { SkillComponent } from './skill/skill.component';
+import { CourseComponent } from './course/course.component';
+import { LanguageComponent } from './language/language.component';
+import { ProfileComponent } from './profile/profile.component';
+import {PersonServiceService} from './person-service.service';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PersonalComponent,
+    ExperienceComponent,
+    EducationComponent,
+    HobbyComponent,
+    SkillComponent,
+    CourseComponent,
+    LanguageComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PersonServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
